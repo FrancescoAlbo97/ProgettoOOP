9795665fdd07adbb4d645bcea429993ad753946b
FrancescoAlbo97 <fralbo97@gmail.com>
2019-07-15 17:26:18 +0200

    finish filter by operator p2

--- src/main/java/com/project/progettoOOP/utils/ParserCSV.java
+++ src/main/java/com/project/progettoOOP/utils/ParserCSV.java
@@ -23,25 +23,18 @@ public class ParserCSV {
      */
     public static ArrayList<Environment> parser (String fileName){
 
-        //List<List<String>> records = new ArrayList<>();
         ArrayList<Environment> arrayList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(COMMA_DELIMITER);
-                //records.add(Arrays.asList(values));
                 for(int i = 1; i < 7; i++){
                     if (values[i].contains("-") && !(values[i].contains("E"))){
                         values[i] = null;
                     }
                 }
                 arrayList.add(new Environment(values[0],values[1],values[2],values[3],values[4],values[5], values[6]));
-                /*
-                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                Date date = formatter.parse(values[0]);
-                arrayList.add(new Environment(date, Float.parseFloat(values[1]), Float.parseFloat(values[2]), Float.parseFloat(values[3]), Float.parseFloat(values[4]), Float.parseFloat(values[5]), Float.parseFloat(values[6])));
-                */
             }
         } finally {
             return arrayList;
