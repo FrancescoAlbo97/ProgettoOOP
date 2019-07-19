8dca0e8b0a3b5299a2ce083b303db311b6b67c01
FrancescoAlbo97 <fralbo97@gmail.com>
2019-07-19 15:46:16 +0200

    Merge branch 'master' of /home/francesco/Scrivania/ProgettoOOP with conflicts.

--- src/main/java/com/project/progettoOOP/model/Environment.java
+++ src/main/java/com/project/progettoOOP/model/Environment.java
@@ -1,6 +1,5 @@
 package com.project.progettoOOP.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
